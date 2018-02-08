ac058d02b61dd4e09de3ea2dc1ff3cdaac5fc031
LAPTOP-UG1B8O19\prate <prateeksurana3255@gmail.com>
2018-02-09 00:56:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+angular.module("smartfarm", [ 'firebase'])
+    // .config(function ($routeProvider) {
+
+    //     $routeProvider
+    //         .when("/", { templateUrl: "views/login.html" })
+    //         .when("/game/:gameId", { templateUrl: "views/game.html" })
+    //         .when("/leaderboard", { templateUrl: "views/leaderboard.html" })
+    //         .otherwise({redirectTo:'/'});
+    // })
+    .controller("FirstCtrl", FirstCtrl)
+   
+    $(window).on("load resize ", function() {
+        var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+        $('.tbl-header').css({'padding-right':scrollWidth});
+      }).resize();
+    
+
+function FirstCtrl($firebaseArray,$firebaseObject) {
+    
+var first =this;
+var usersRef=firebase.database().ref("users");
+first.users=$firebaseArray(usersRef);
+var cropsRef=firebase.database().ref("crop");
+first.crops=$firebaseObject(cropsRef);
+
+
+}
+
